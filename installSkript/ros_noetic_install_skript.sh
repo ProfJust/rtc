72bfd782cba9beb05c6f994e03d1ca8ca80f44ee
Prof. Just <olaf.just@w-hs.de>
2020-08-24 19:23:04 +0200

    Update ros_noetic_install_skript.sh

--- installSkript/ros_noetic_install_skript.sh
+++ installSkript/ros_noetic_install_skript.sh
@@ -35,7 +35,7 @@ rosdep update
 echo -e "\033[34m ---------- Erstelle catkin_ws  ------------ \033[0m "
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-git clone https://github.com/ProfJust/emr.git
+git clone https://github.com/ProfJust/rtc.git
 
 cd ~/catkin_ws/
 catkin_make
