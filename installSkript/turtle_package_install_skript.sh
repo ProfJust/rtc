53f9179491294c5c2aca3be4abb33a9892baf29c
Prof. Just <olaf.just@w-hs.de>
2020-08-24 20:08:47 +0200

    Add files via upload

---
+++ installSkript/turtle_package_install_skript.sh
@@ -0,0 +1,59 @@
+# turtle_package_install_skript.sh
+# Installation der TurtleBot 3 Pakete auf dem Remote-PC
+# https://emanual.robotis.com/docs/en/platform/turtlebot3/overview/
+# OJ fuer robotik.bocholt@w-hs.de
+# WS2020
+
+#!/bin/bash
+# script to setup youbot-Workspace
+
+echo -e "\033[34m ---------- RTC WS20 - TurtleBot Workspace einrichten  ------------ \033[0m "
+
+echo "Shellskript zur Installation der Treiber-Pakete" 
+
+sudo apt-get dist-upgrade -y
+pwd
+cd ~/catkin_ws/src/
+
+sudo apt-get install ros-noetic-joy  -y
+sudo apt-get install ros-noetic-teleop-twist-joy -y
+sudo apt-get install ros-noetic-teleop-twist-keyboard -y
+sudo apt-get install ros-noetic-laser-proc -y
+sudo apt-get install ros-noetic-rgbd-launch -y
+sudo apt-get install ros-noetic-depthimage-to-laserscan -y
+sudo apt-get install ros-noetic-rosserial-arduino -y
+sudo apt-get install ros-noetic-rosserial-python -y
+sudo apt-get install ros-noetic-rosserial-server -y
+sudo apt-get install ros-noetic-rosserial-client -y
+sudo apt-get install ros-noetic-rosserial-msgs -y
+sudo apt-get install ros-noetic-amcl -y
+sudo apt-get install ros-noetic-map-server -y
+sudo apt-get install ros-noetic-move-base -y
+sudo apt-get install ros-noetic-urdf -y
+sudo apt-get install ros-noetic-xacro -y
+sudo apt-get install ros-noetic-compressed-image-transport -y
+sudo apt-get install ros-noetic-rqt-image-view -y
+sudo apt-get install ros-noetic-gmapping -y
+sudo apt-get install ros-noetic-navigation -y
+sudo apt-get install ros-noetic-interactive-markers -y
+
+echo -e "\033[34m Installiere die TurtleBot3-Pakete \033[0m"
+cd ~/catkin_ws/src/
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone -b melodic-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
+echo -e "\033[34m Setze den TurtleBot3-Typ auf Burger \033[0m"
+export TURTLEBOT3_MODEL=burger
+source devel/setup.bash
+
+echo -e "\033[34m Aktualisiere alle Abhaengigkeiten der ROS-Pakete \033[0m"
+rosdep update
+rosdep install --from-paths src --ignore-src -r -y
+
+#echo -e "\033[34m to do:   $ cd ~/catkin_ws/  ...   catkin_make \033[0m"
+cd ~/catkin_ws/
+catkin_make
+
+echo -e "\033[34m RTC - WS20 - Workspace is installed - have fun!  \033[0m"
+echo -e "\033[32m $ roslaunch turtlebot3_fake turtlebot3_fake.launch \033[0m"
+
