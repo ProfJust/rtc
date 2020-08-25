2d7c30dac9db17007631ec5dc9431f96bdbaf039
Prof. Just <olaf.just@w-hs.de>
2020-08-25 13:34:54 +0200

    Add files via upload

--- installSkript/ros_noetic_install_skript.sh
+++ installSkript/ros_noetic_install_skript.sh
@@ -1,7 +1,7 @@
 # ros_noetic_install_skript.sh
 # ROS Noetic auf einem Rechner mit Ubuntu 20.04 Focal Fossa  installieren
 # OJ fuer robotik.bocholt@w-hs.de
-# WS2020
+# WS2020 25.8.20202
 
 #!/bin/bash
 # script to setup your catkin_ws-Workspace
