320eede54f85f1c4ea43595291ffa9345e2b2c9c
ProfJust <olaf.just@w-hs.de>
2021-02-10 11:15:20 +0100

    Fixed Conversion Error in sonar.h

--- turtleBot_core_openCr_arduino/turtlebot3/examples/turtlebot3_burger/turtlebot3_core/sonar.h
+++ turtleBot_core_openCr_arduino/turtlebot3/examples/turtlebot3_burger/turtlebot3_core/sonar.h
@@ -8,12 +8,14 @@
 // auszuführen 
 #include "turtlebot3_core_config.h"
 
-const int UPDATE_INTERVALL= 750; //ms
+const int UPDATE_INTERVALL= 500; //ms
 // Schallgeschwindigkeit 343,2 m/s 
   // 58ms pro m hin und zurück
   // Sonar < 2m => Timeout 11600 us
   // Sonar < 3m => Timeout 17400 us
-const unsigned long int TIMEOUT=50000;
+const uint32_t TIMEOUT=17500; //us
+// ...dazu passend der maximale Weg
+const uint16_t  MAX_DIST=3000; //mm
 
   //-- Abstands-Messung mit 2x SR04-Sonar durchfuehren --
   //so KLAPPTS => rostopic hz /tf ~28 Hz bei 35cm
@@ -65,7 +67,7 @@ void updateSonar(uint32_t now)
         //pulseIn mit Abbruch bei zu langer Wartezeit
         duration_uint = pulseIn(sonar_pin_.echo, HIGH, TIMEOUT);
         distance_uint = ((340 * duration_uint) / 10000) / 2;
-        if (distance_uint < 300) /*mm*/
+        if (distance_uint <= MAX_DIST) /*mm*/
             sonar_data_ = (float) distance_uint; //to global float Var
         else 
             sonar_data_ = 0.0;
@@ -89,8 +91,8 @@ void updateSonar2(uint32_t now)
 
         //pulseIn mit Abbruch bei zu langer Wartezeit
         duration_uint = pulseIn(sonar_pin2_.echo, HIGH, TIMEOUT);
-        distance_uint = ((float)(340 * duration_uint) / 10000) / 2;
-        if (distance_uint < 300) /*mm*/
+        distance_uint = ((340 * duration_uint) / 10000) / 2;
+        if (distance_uint <= MAX_DIST) /*mm*/
             sonar_data2_ = (float) distance_uint; //to global float Var
         else 
             sonar_data2_ = 0.0;
