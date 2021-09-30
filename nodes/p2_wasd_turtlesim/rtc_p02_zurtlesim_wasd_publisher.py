ced66d41b03b3504b85cb9b1261310c34397e9ed
ProfJust <olaf.just@w-hs.de>
2021-09-30 17:48:49 +0200

    ws21

---
+++ nodes/p2_wasd_turtlesim/rtc_p02_zurtlesim_wasd_publisher.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# --- rtc_p02_turtlesim_wasd_publisher.py ------
+# Version vom 30.9.2021 by OJ
+# -----------------------------
+import rospy
+from geometry_msgs.msg import Twist
+# from turtlesim.msg import Pose
+
+
+# Einzelnes Zeichen lesen mit getch()
+# https://www.raspberrypi.org/forums/viewtopic.php?t=69046
+def getch():
+    import sys
+    import termios
+    old_settings = termios.tcgetattr(0)
+    new_settings = old_settings[:]
+    new_settings[3] &= ~termios.ICANON
+    try:
+        termios.tcsetattr(0, termios.TCSANOW, new_settings)
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(0, termios.TCSANOW, old_settings)
+    return ch
+
+
+# Publisher which will publish to the topic '/turtle1/cmd_vel'.
+velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+vel_msg = Twist()
+vel_msg.linear.x = 0
+vel_msg.linear.y = 0
+vel_msg.linear.z = 0
+vel_msg.angular.z = 0
+vel_msg.angular.x = 0
+vel_msg.angular.y = 0
+
+
+def set_vel():
+    c = getch()
+    if c == 'w':
+        vel_msg.linear.x = 0.1
+    elif c == 's':
+        vel_msg.linear.x = -0.1
+    elif c == 'a':
+        vel_msg.angular.z = 0.1
+    elif c == 'd':
+        vel_msg.angular.z = -0.1
+    print("\b")  # Zeichen löschen
+
+
+if __name__ == '__main__':
+    # Node wird vereinbart
+    rospy.init_node('wasd_node', anonymous=True)
+    rate = rospy.Rate(50)  # 50hz
+
+    try:
+        while not rospy.is_shutdown():
+            set_vel()
+            velocity_publisher.publish(vel_msg)
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        rospy.loginfo(" ROS Fehler ")
+        pass
--- scripts/HelloPython/bsp02_WHILE.py
+++ scripts/HelloPython/bsp02_WHILE.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-# bsp01_WHILE.py	
-#-----------------------------------
-# 19.09.2018 by OJ
-#----------------------------------- 
+# bsp01_WHILE.py
+# -----------------------------------
+# 30.09.2021 by OJ
+# -----------------------------------
 
-#---- Variablen ----
+# ---- Variablen ----
 i = 0
-#print("Zahl  2.Pot  3.Pot");
-while(i<=9):
-	#print("Zahl %02d 2.Pot %03d 3.Pot %04d" %( i , i**2, i**3) );	
-	print("Zahl {0:02d} 2.Pot {1:03d} 3.Pot {2:04d}".format( i , i**2, i**3) );	
-	i+=1; # i++ gibt es nicht!
+
+while(i <= 9):
+    print("Zahl {0:02d}".format(i),
+          "Zahl {0:03d}".format(i**2),
+          "Zahl {0:04d}".format(i**3))
+    i += 1  # i++ gibt es nicht!
 
 print("Schleife zu Ende")
 
