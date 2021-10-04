2cc5ac353d2b4b6a1294294dc4df84b278e0866d
ProfJust <olaf.just@w-hs.de>
2021-10-04 18:50:35 +0200

    Heute

--- scripts/HelloPython/bsp01_FOR.py
+++ scripts/HelloPython/bsp01_FOR.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # bsp01_FOR.py
 # -----------------------------------
-# 19.09.2018 by OJ
+# 4.10.2021 by OJ
 # -----------------------------------
 
 # ---- Variablen ----
@@ -9,7 +9,7 @@ text = "Willkommen im Robotik Labor"
 i = 0
 for c in text:
     print("Zeichen Nr. ", i, " ", text[i], " ", c)
-    i += 1 # i++ gibt es nicht!
+    i += 1  # i++ gibt es nicht!
 
     print(" For-Schleife zu Ende")
 
