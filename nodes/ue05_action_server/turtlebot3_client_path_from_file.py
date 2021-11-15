d906b45628ba15592128f7ff7c7f1facd2437dcd
ProfJust <olaf.just@w-hs.de>
2021-11-15 17:41:16 +0100

    *py

--- nodes/ue05_action_server/turtlebot3_client_path_from_file.py
+++ nodes/ue05_action_server/turtlebot3_client_path_from_file.py
@@ -3,8 +3,8 @@
 # edited WHS, OJ , 12.11.2021   #
 # usage
 #    $1 roslaunch turtlebot3_gazebo turtlebot3_empty_world.launch
-#    $2 rosrun rtc turtlebot3_server_path
-#    $3 rosrun rtc turtlebot3_client_path
+#    $2 rosrun rtc turtlebot3_server_path.py
+#    $3 rosrun rtc turtlebot3_client_path_from_file.py
 
 import rospy
 import actionlib
