d3d80db8e1c091708b93afbe595237442aeae12f
ProfJust <olaf.just@w-hs.de>
2022-10-14 18:41:18 +0200

    WS22

--- nodes/ue02/moveTurtle_distance.py
+++ nodes/ue02/moveTurtle_distance.py
@@ -27,8 +27,7 @@ def update_pose(data):
     # rospy.loginfo(rospy.get_caller_id() + "x %s  y %s  theta %s",
     #                                   data.x, data.y, data.theta)
     pose.x = round(data.x, 4)  # Runde auf 4 Stellen
-    pose.y = round(data.y, 4)
-    pose.theta = round(data.theta,  4)
+    ### HIER CODE EINFUEGEN ###
 
 
 def move():
@@ -36,22 +35,22 @@ def move():
     # unique node (using anonymous=True).
     rospy.init_node('turtlebot_controller', anonymous=True)
 
-    # Publisher which will publish to the topic '/turtle1/cmd_vel'.
-    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel',
-                                         Twist,
+    # Publisher which will publish to the Twist topic '/turtle1/cmd_vel'.
+    velocity_publisher = rospy.Publisher('### HIER CODE EINFUEGEN ###',
+                                         ### HIER CODE EINFUEGEN ###,
                                          queue_size=10)
     # A subscriber to the topic '/turtle1/pose'. self.update_pose is called
     # when a message of type Pose is received.
-    rospy.Subscriber('/turtle1/pose',  Pose, update_pose)
+    rospy.Subscriber('### HIER CODE EINFUEGEN ###',  Pose, ### HIER CODE EINFUEGEN ###)
     rate = rospy.Rate(10)
 
     # Get the input from the user.
     dist_x = eval(input("Set your x dist: "))
-    dist_y = eval(input("Set your y dist: "))
+    dist_y = ### HIER CODE EINFUEGEN ###
 
     # Wegstrecke und Orientierung der Turtle berechnen
-    dist = sqrt(pow(dist_x, 2) + pow(dist_y, 2))
-    sollTheta = atan2(dist_y, dist_x)
+    dist = ### HIER CODE EINFUEGEN ###
+    sollTheta = ### HIER CODE EINFUEGEN ###
 
     # Get start pose of Turtle - meanwhile received?
     start_x = pose.x
@@ -68,39 +67,36 @@ def move():
     tolerance = 0.015
     while (abs(pose.theta - sollTheta) > tolerance):
         # theta auf Bereich [-pi...pi] begrenzen
-        if pose.theta > pi:
-            pose.theta = pose.theta - 2 * pi
-        elif pose.theta < -pi:
-            pose.theta = pose.theta + 2 * pi
+        ### HIER CODE EINFUEGEN ###
 
         # set Angular velocity in the z-axis.
         if pose.theta - sollTheta > 0:
             vel_msg.angular.z = -0.4
         else:
             vel_msg.angular.z = 0.4
+
         # Debug ausgabe
         rospy.loginfo("Pose is %s", pose.theta)
         rospy.loginfo("Goal angle is %s", sollTheta)
         rospy.loginfo("Still to turn %s ", abs(pose.theta - sollTheta))
 
-        velocity_publisher.publish(vel_msg)  # Publishing our vel_msg
+        velocity_publisher.publish(### HIER CODE EINFUEGEN ###)  # Publishing our vel_msg
         rate.sleep()  # Publish at the desired rate
 
     # Stopping our robot after the movement is over
     vel_msg.linear.x = 0
     vel_msg.angular.z = 0
-    velocity_publisher.publish(vel_msg)
+    velocity_publisher.publish(### HIER CODE EINFUEGEN ###)
 
     # --- Dann die Strecke fahren ---
-    while sqrt(pow((start_x - pose.x), 2)
-               + pow((start_y - pose.y), 2)) < abs(dist):
+    while ### HIER CODE EINFUEGEN ###:
 
         # Linear velocity in the x-axis.
-        vel_msg.linear.x = 0.2
+        ### HIER CODE EINFUEGEN ###
 
         # Publishing our vel_msg
 
-        velocity_publisher.publish(vel_msg)  # Publishing our vel_msg
+        velocity_publisher.publish(### HIER CODE EINFUEGEN ###)  # Publishing our vel_msg
 
         rospy.loginfo("Pose is %s %s", pose.x, pose.y)
         rospy.loginfo("Still to Go %s ",
