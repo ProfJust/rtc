0afa2bdb34f50f7d96f619c1b66dab067fa6dec4
ProfJust <olaf.just@w-hs.de>
2022-10-24 19:33:54 +0200

    Plenum WS22

--- nodes/ue03/TurtleClassFile.py
+++ nodes/ue03/TurtleClassFile.py
@@ -11,6 +11,7 @@ class TurtleBotClass:
         self.goal = Pose()
         self.vel_msg = Twist()
 
+        self.objektname = turtleName
         # Creates a node with name 'turtlebot_controller' and make sure it is a
         # unique node (using anonymous=True).
         try:
@@ -18,7 +19,7 @@ class TurtleBotClass:
         except rospy.ROSInterruptException:
             pass
 
-        zk = turtleName + '/cmd_vel'
+        zk = turtleName + '/cmd_vel'  # eine Zeichenkette
         # Publisher which will publish to the topic '/turtle1/cmd_vel'.
         self.velocity_publisher = rospy.Publisher(zk,
                                                   Twist, queue_size=10)
@@ -41,17 +42,21 @@ class TurtleBotClass:
         input("Hit any Key to start")
 
     def getGoalFromUser(self):
-        self.stop_robot()
+        # self.stop_robot()
+        print(self.objektname)
         self.goal.x = eval(input("Set your x goal: "))
         self.goal.y = eval(input("Set your y goal: "))
         # goal.theta not used
+        if self.goal.x < 0 or self.goal.y < 0:
+            input("Keine negativen Werte erlaubt - hit any key")
+            exit(1)
 
     def update_pose(self, data):  # CallbacK für Pose
         """Callback function which is called when a new message of type Pose is
         received by the subscriber."""
         self.pose = data
-        self.pose.x = round(self.pose.x, 5)
-        self.pose.y = round(self.pose.y, 5)
+        self.pose.x = round(self.pose.x, 4)
+        self.pose.y = round(self.pose.y, 4)
 
     def euclidean_distance(self):
         """Euclidean distance between current pose and the goal."""
@@ -69,10 +74,11 @@ class TurtleBotClass:
     def steering_angle(self):
         return atan2(self.goal.y - self.pose.y, self.goal.x - self.pose.x)
 
-    def set_angular_vel(self, constant=3.0, ang_max=1.5):
+    def set_angular_vel(self, constant=3.0, ang_vel_max=1.5):
         ang_vel = constant * (self.steering_angle() - self.pose.theta)
-        if(ang_vel > ang_max):
-            ang_vel = ang_max
+        if(ang_vel > ang_vel_max):
+            ang_vel = ang_vel_max
+        # Message to publish
         self.vel_msg.angular.z = ang_vel
         self.vel_msg.angular.x = 0
         self.vel_msg.angular.y = 0
@@ -80,11 +86,12 @@ class TurtleBotClass:
     def stop_robot(self):
         # Stopping our robot after the movement is over.
         rospy.loginfo(" ######  Goal reached, Stop Robot #######")
+        # Message to publish
         self.vel_msg.linear.x = 0
         self.vel_msg.angular.z = 0
         self.velocity_publisher.publish(self.vel_msg)
 
-    def goal_reached(self, distance_tolerance=0.1):
+    def is_goal_reached(self, distance_tolerance=0.1):
         if self.euclidean_distance() < distance_tolerance:
             return True
         else:
@@ -101,7 +108,7 @@ class TurtleBotClass:
     def move2goal(self):
         # Moves the turtle to the goal
 
-        while not self.goal_reached():
+        while not self.is_goal_reached():
             # Linear velocity in the x-axis.
             self.set_linear_vel()
             # Angular velocity in the z-axis.
@@ -109,7 +116,7 @@ class TurtleBotClass:
             # Publishing our vel_msg
             self.velocity_publisher.publish(self.vel_msg)
             # Publish at the desired rate.
-            self.rate.sleep()
+            # self.rate.sleep()  <===== Wieso Fehler ????
             # debug Info
             self.pose_speed_info()
 
