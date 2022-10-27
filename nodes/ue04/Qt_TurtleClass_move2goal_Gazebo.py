961bfedf618c78f6bba0a8add30632e89bd99fe2
ProfJust <olaf.just@w-hs.de>
2022-10-27 11:21:15 +0200

    ws22

--- nodes/ue04/Qt_TurtleClass_move2goal_Gazebo.py
+++ nodes/ue04/Qt_TurtleClass_move2goal_Gazebo.py
@@ -11,6 +11,10 @@
 import sys
 import rospy
 from TurtleBotClassFile import TurtleBotClass
+# Falls der Import nicht klappt, checke PYTHONPATH
+# in der .bashrc (Skript Kapitel 11.7)
+# export PYTHONPATH=$PYTHONPATH:~/catkin_ws/src/rtc/rtc_dist-packages
+
 # Qt -------------------------------
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import (QWidget, QLCDNumber, QSlider,
