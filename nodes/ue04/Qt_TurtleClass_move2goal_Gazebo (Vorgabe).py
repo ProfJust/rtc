87df111cde6559d2636f12114c54c2c76e17093c
ProfJust <olaf.just@w-hs.de>
2022-10-31 18:53:27 +0100

    plenum 31.10.22

--- nodes/ue04/Qt_TurtleClass_move2goal_Gazebo (Vorgabe).py
+++ nodes/ue04/Qt_TurtleClass_move2goal_Gazebo (Vorgabe).py
@@ -42,18 +42,28 @@ class TurtleUIClass(QWidget):
         LCDstartWert = 0
         self.lcdX = QLCDNumber(self)
         self.lcdX.display(LCDstartWert)
+        self.lcdY = QLCDNumber(self)
+        self.lcdY.display(LCDstartWert)
         
         self.sldX = QSlider(Qt.Horizontal, self)
         self.sldX.setMaximum(6)
         self.sldX.setMinimum(-6)
         self.sldX.setValue(LCDstartWert)
+
+        self.sldY = QSlider(Qt.Horizontal, self)
+        self.sldY.setMaximum(6)
+        self.sldY.setMinimum(-6)
+        self.sldY.setValue(LCDstartWert)
         
         self.pbLessX = QPushButton('<')
         self.pbMoreX = QPushButton('>')
+        self.pbLessY = QPushButton('<')
+        self.pbMoreY = QPushButton('>')
         self.pbGo = QPushButton(' Go Turtle ')
         self.pbStop = QPushButton(' Stop ')
 
         self.lblInfoX = QLabel('X-Goal (world coordinates)')
+        self.lblInfoY = QLabel('Y-Goal (world coordinates)')
         self.lblStatus = QLabel('Status - Ausgabe')
 
         # BOX-Layout mit Widgets füllen
@@ -61,20 +71,25 @@ class TurtleUIClass(QWidget):
         #  0.Reihe - Label
         hbox = QHBoxLayout()
         hbox.addWidget(self.lblInfoX)
+        hbox.addWidget(self.lblInfoY)
         vbox.addLayout(hbox)
         # 1.Reihe
         hbox = QHBoxLayout()
         hbox.addWidget(self.lcdX)
+        hbox.addWidget(self.lcdY)
         vbox.addLayout(hbox)
         # 2.Reihe
         hbox = QHBoxLayout()
         hbox.addWidget(self.sldX)
+        hbox.addWidget(self.sldY)
         vbox.addLayout(hbox)
 
         # 3.Reihe
         hbox = QHBoxLayout()
         hbox.addWidget(self.pbLessX)
         hbox.addWidget(self.pbMoreX)
+        hbox.addWidget(self.pbLessY)
+        hbox.addWidget(self.pbMoreY)
         vbox.addLayout(hbox)
 
         # 4.Reihe
@@ -102,9 +117,12 @@ class TurtleUIClass(QWidget):
 
         # Signal und Slot verbinden
         self.sldX.valueChanged.connect(self.lcdX.display)
+        self.sldY.valueChanged.connect(self.lcdY.display)
 
         self.pbLessX.clicked.connect(self.SlotKlickX)
         self.pbMoreX.clicked.connect(self.SlotKlickX)
+        self.pbLessY.clicked.connect(self.SlotKlickY)
+        self.pbMoreY.clicked.connect(self.SlotKlickY)
         self.pbGo.clicked.connect(self.SlotGo)
         self.pbStop.clicked.connect(self.SlotStop)
 
@@ -119,12 +137,24 @@ class TurtleUIClass(QWidget):
             wert = self.sldX.value()
             wert = wert+1
             self.sldX.setValue(wert)
+    
+    def SlotKlickY(self):
+        sender = self.sender()
+        self.lblStatus.setText(' Y ' + sender.text() + ' was pressed')
+        if sender.text() == '<':
+            wert = self.sldY.value()
+            wert = wert-1
+            self.sldY.setValue(wert)
+        else:
+            wert = self.sldY.value()
+            wert = wert+1
+            self.sldY.setValue(wert)
 
     def SlotGo(self):
         self.Stop = False
         """ Hier geht die Turtle ab """
         turtle1.goal.x = self.sldX.value()
-        turtle1.goal.y = 0
+        turtle1.goal.y = self.sldY.value()
         self.timer.start(20)  # 20 msec
 
     def SlotStop(self):
